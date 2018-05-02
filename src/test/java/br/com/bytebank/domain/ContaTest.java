9231cb5d997169512154e424978992dd06cfdc82
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-05-02 17:30:00 -0300

    Implementando caso de teste para a função depositar.

--- src/test/java/br/com/bytebank/domain/ContaTest.java
+++ src/test/java/br/com/bytebank/domain/ContaTest.java
@@ -15,4 +15,15 @@ public class ContaTest {
         assertEquals(2322, conta.getAgencia());
         assertEquals(331213, conta.getConta());
     }
+    
+    
+    @Test
+    public void testRealizandoDeposito(){
+        Cliente cliente = new Cliente("Fabricio Oliveira", "417.481.378.75");
+        Conta conta = new Conta(cliente, 2322, 331213);
+        
+        conta.depositar(1000.00);
+        
+        assertEquals(1000.00, conta.getSaldo(), 0.001);
+    }
 }
